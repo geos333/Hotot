b39478d293a801e95ba6366984e5a776967f22c7
geos333 <l-geos-x@inbox.ru>
2013-07-22 12:26:34 +0400

    test

---
+++ Hotot/Hotot/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Hotot
+//
+//  Created by geos on 22.07.13.
+//  Copyright (c) 2013 geos. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Hotot/Hotot/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Hotot
+//
+//  Created by geos on 22.07.13.
+//  Copyright (c) 2013 geos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
