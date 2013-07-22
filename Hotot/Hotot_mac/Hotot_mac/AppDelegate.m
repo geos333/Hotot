e510b2f588dd28a5389206287ad174071383863c
geos333 <l-geos-x@inbox.ru>
2013-07-22 12:46:33 +0400

    Clean project

---
+++ Hotot/Hotot_mac/Hotot_mac/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Hotot_mac
+//
+//  Created by geos on 22.07.13.
+//  Copyright (c) 2013 geos. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
